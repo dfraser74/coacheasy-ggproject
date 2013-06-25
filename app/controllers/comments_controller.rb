77d668f61de81b16bde52da466d1737731d5728a
joefox <joefox@blueyonder.co.uk>
2013-06-25 18:24:34 +0100

    Added edit and delete features to comments and tidied up some coding for the programs model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,41 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = @program.comments.build
+  end 
+  
   def create
     @program = Program.find(params[:program_id])
     @comment = @program.comments.create(params[:comment])
     redirect_to program_path(@program)
   end
 
+  def destroy
+    @program = Program.find(params[:program_id])
+    @comment = @program.comments.find(params[:id])
+    @comment.destroy
+    redirect_to program_path(@program)
+  end
+  
+  def edit
+    @program = Program.find(params[:program_id])
+    @comment = @program.comments.find(params[:id])
+  end
+  
+   def update
+    @program = Program.find(params[:program_id])
+    @comment = @program.comments.find(params[:id])
+
+    respond_to do |format|
+      #if @program.comments.update_attributes(params[:comment])
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to program_path(@program), notice: 'Comment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+      end
+    end
+    
 end
