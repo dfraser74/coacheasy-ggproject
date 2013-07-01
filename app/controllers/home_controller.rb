c686e3f0ced0099f194b4461086d3c8969bb1a91
joefox <joefox@blueyonder.co.uk>
2013-07-01 14:16:00 +0100

    website now looking nicer still not finished yet, still have navigation system to add, still also need to finish off some functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,15 @@
 class HomeController < ApplicationController
+  layout 'homelayout'
   
   def index
     if coach_signed_in?
        redirect_to :controller => 'dashboard', :action => 'index'
     end
+    
   end
   
+  def create
+    render :layout => 'homelayout'
+  end
 end
+
